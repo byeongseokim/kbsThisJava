12b67e8e38144e352dc21dc3a55162ed0b78ad50
unknown <kbs970318@gmail.com>
2023-04-12 16:12:54 +0900

    출력테스트

---
+++ ThisJava_git/src/ch00/sec00/Test01.java
@@ -0,0 +1,16 @@
+package ch00.sec00;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// 문자 출력
+		System.out.println("Hello Java");
+		System.out.println("한글자바");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+	}
+
+}
